45d6d57638593b0a08a05ecc2521dce6c80ec0bf
715686280@qq.com <qq715686280>
2018-05-15 13:27:24 +0800

    5.15 commit

--- app/src/main/java/com/zjdt/dtsjwb/Activity/CAssetsRActivity.java
+++ app/src/main/java/com/zjdt/dtsjwb/Activity/CAssetsRActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import com.zjdt.dtsjwb.R;
 
 public class CAssetsRActivity extends AppCompatActivity {
-    //资产登记
+    //资产登记  通过碎片 登记机房的多个系统部件
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cassets_r);
     }
+
+
+
 }
---
+++ app/src/main/java/com/zjdt/dtsjwb/fragment/AirAssit.java
@@ -0,0 +1,6 @@
+package com.zjdt.dtsjwb.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class AirAssit extends Fragment {
+}
